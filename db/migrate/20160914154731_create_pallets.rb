a625dbf9ffd592e5e3dc337e065f2430bdc806f0
aaaa1379 <aaaa1379@gmail.com>
2016-09-15 00:31:23 +0800

    add title and description

--- db/migrate/20160914154731_create_pallets.rb
+++ db/migrate/20160914154731_create_pallets.rb
@@ -1,8 +1,12 @@
-class Pallets < ActiveRecord::Migration[5.0]
+class CreatePallets < ActiveRecord::Migration[5.0]
   def change
     create_table :pallets, id: :uuid  do |t|
       # relation to which user
       t.uuid :user_id
+
+      # pallet information
+      t.string :title
+      t.string :description
 
       # save color informations
       t.integer :colors_count
