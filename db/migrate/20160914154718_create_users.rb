b4052f89b53c7f49f40fbd3e0951394014002206
aaaa1379 <aaaa1379@gmail.com>
2016-09-16 04:39:18 +0800

    change user name to username

--- db/migrate/20160914154718_create_users.rb
+++ db/migrate/20160914154718_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users, id: :uuid  do |t|
       # username
-      t.string :name
+      t.string :username
 
       t.timestamps null: false
     end
