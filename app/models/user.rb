9bbb5fcdf596750ec496adf184ef10967635a505
aaaa1379 <aaaa1379@gmail.com>
2016-09-16 04:45:46 +0800

    add devise settings and username validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,
+         :validatable#, :confirmable, :lockable, :timeoutable#, :omniauthable
   has_many :pallets
+
+  # Only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 end
