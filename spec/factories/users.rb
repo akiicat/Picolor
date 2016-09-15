9bb138d30b51a6c6a21bc465e5e68b85c4c688a9
aaaa1379 <aaaa1379@gmail.com>
2016-09-16 04:54:10 +0800

    factory users change user to username

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,15 @@ FactoryGirl.define do
   sequence :name do |n|
     "Cat#{n}"
   end
+  sequence :email do |n|
+    "email#{n}@example.com"
+  end
 
   factory :user do
-    name { generate(:name) }
+    username { generate(:name) }
+    email { generate(:email) }
+    password "123456"
+    password_confirmation "123456"
 
     factory :user_with_pallets do
       transient do
