2cc607ae312871394ff4269597089aa60c57f756
aaaa1379 <aaaa1379@gmail.com>
2016-09-17 01:03:18 +0800

    pallet controller

---
+++ app/controllers/pallets_controller.rb
@@ -0,0 +1,76 @@
+class PalletsController < ApplicationController
+  before_action :set_pallet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  # GET /pallets
+  # GET /pallets.json
+  def index
+    @pallets = Pallet.all
+  end
+
+  # GET /pallets/1
+  # GET /pallets/1.json
+  def show
+  end
+
+  # GET /pallets/new
+  def new
+    @pallet = Pallet.new
+  end
+
+  # GET /pallets/1/edit
+  def edit
+  end
+
+  # POST /pallets
+  # POST /pallets.json
+  def create
+    @pallet = Pallet.new(pallet_params)
+    @pallet.user_id = current_user.id
+    byebug
+    respond_to do |format|
+      if @pallet.save
+        format.html { redirect_to @pallet, notice: 'Pallet was successfully created.' }
+        format.json { render :show, status: :created, location: @pallet }
+      else
+        format.html { render :new }
+        format.json { render json: @pallet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /pallets/1
+  # PATCH/PUT /pallets/1.json
+  def update
+    respond_to do |format|
+      if @pallet.update(pallet_params)
+        format.html { redirect_to @pallet, notice: 'Pallet was successfully updated.' }
+        format.json { render :show, status: :ok, location: @pallet }
+      else
+        format.html { render :edit }
+        format.json { render json: @pallet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /pallets/1
+  # DELETE /pallets/1.json
+  def destroy
+    @pallet.destroy
+    respond_to do |format|
+      format.html { redirect_to pallets_url, notice: 'Pallet was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pallet
+      @pallet = Pallet.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pallet_params
+      params.require(:pallet).permit(:title, :description, :colors_count, :colors)
+    end
+end
