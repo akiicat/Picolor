549969963fc4099a093304ed35b79129b0415919
aaaa1379 <aaaa1379@gmail.com>
2016-10-12 05:07:32 +0800

    update

--- app/assets/javascripts/imageUpload.js
+++ app/assets/javascripts/imageUpload.js
@@ -1,5 +1,4 @@
-var canvas_hash = new Object();
-var canvas_test = new Object();
+var canvas_list = new Object();
 
 $(document).ready(function(){
   $('[data-url][data-id]').each(function(){
@@ -7,12 +6,28 @@ $(document).ready(function(){
     var id  = $(this).data('id');
 
     var _this = $(this);
-    loadImage(id, url, _this);
+    createList(id, url, _this);
   });
 
-  function loadImage(id, url, _this = null){
-    var img = new Image();
-    img.onload = function(){
+  $('[data-url][data-id]').click(function(){
+    var id = $(this).data('id');
+    loadImage(id);
+  });
+
+  $('#dz-canvas').click(function(e) {
+    var canvas = $('#dz-canvas > canvas').get(0);
+    var bgColor = pixelColor(canvas, e.pageX, e.pageY);
+
+    $('body').css('backgroundColor', bgColor);
+    console.log('set bg color: ' + bgColor);
+  });
+
+  $('#rb-images').sortable();
+  $('#rb-colors').sortable();
+
+  function createList(id, url, _this = null){
+    var image = new Image();
+    image.onload = function(){
       var canvas = document.createElement('canvas');
       var context = canvas.getContext('2d');
 
@@ -21,54 +36,51 @@ $(document).ready(function(){
 
       context.drawImage(this, 0, 0);
 
-      canvas_hash[id] = canvas.toDataURL();
-      canvas_test[id] = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-
+      canvas_list[id] = canvas.toDataURL();
       _this.append(canvas);
     };
-    img.crossOrigin = 'Anonymous';
-    img.src = url;
+    image.onerror = function(){
+      // sth
+    }
+    image.crossOrigin = 'Anonymous';
+    image.src = url;
   }
 
-  $('[data-url][data-id]').click(function(){
-    var id    = $(this).data('id');
-    var src   = canvas_hash[id];
-    var src2  = canvas_test[id];
-
+  function loadImage(id){
+    var src = canvas_list[id];
     var canvas  = document.createElement('canvas');
     var context = canvas.getContext('2d');
 
-    var img = new Image();
-    img.src = src;
-    img.alt = 'hi';
-    img.onload = function(){
+    var image = new Image();
+    image.onload = function(){
       canvas.width = this.width;
       canvas.height = this.height;
 
       context.drawImage(this, 0, 0);
-      console.log(this.width, this.height);
     };
-    //console.log(img);
+    image.src = src;
 
     $('#dz-canvas').html(canvas);
+  }
 
-    //console.log(id, src, src2, canvas);
-  });
+  function pixelColor(canvas, x, y){
+    var context = canvas.getContext('2d');
 
-  $("#dz-canvas").click(function(e) {
-    var canvas = $("#dz-canvas").children('canvas').get(0);
-    var ctx = canvas.getContext("2d");
+    var srcW = canvas.width;
+    var srcH = canvas.height;
+    var rszW = parseInt($(canvas).css('width'));
+    var rszH = parseInt($(canvas).css('height'));
+    var ratio = (srcW + srcH) / (rszW + rszH);
 
     var canvasOffset = $(canvas).offset();
-    var canvasX = Math.floor(e.pageX - canvasOffset.left);
-    var canvasY = Math.floor(e.pageY - canvasOffset.top);
+    var canvasX = Math.floor((x - canvasOffset.left) * ratio);
+    var canvasY = Math.floor((y - canvasOffset.top) * ratio);
 
-    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var imageData = context.getImageData(0, 0, srcW, srcH);
     var pixels = imageData.data;
-    var pixelRedIndex = ((canvasY - 1) * (imageData.width * 4)) + ((canvasX - 1) * 4);
-    var pixelcolor = "rgba("+pixels[pixelRedIndex]+", "+pixels[pixelRedIndex+1]+", "+pixels[pixelRedIndex+2]+", "+pixels[pixelRedIndex+3]+")";
+    var index = ((canvasY - 1) * (imageData.width * 4)) + ((canvasX - 1) * 4);
+    var pixelcolor = 'rgba(' + pixels[index] + ', ' + pixels[index + 1] + ', ' + pixels[index + 2] + ', ' + pixels[index + 3] + ')';
 
-    $("body").css("backgroundColor", pixelcolor);
-    console.log('set bg color: ' + pixelcolor);
-  });
+    return pixelcolor;
+  }
 })
